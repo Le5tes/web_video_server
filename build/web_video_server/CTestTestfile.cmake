1db3ffece96dcfb74822b20da8b729bc9d6fc967
Tim Williamson <tim.williamson@kaluza.com>
2022-06-29 20:16:12 +0100

    add built files

---
+++ build/web_video_server/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/timwilliamson/dev/personal/Robotics/web_video_server
+# Build directory: /home/timwilliamson/dev/personal/Robotics/web_video_server/build/web_video_server
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
